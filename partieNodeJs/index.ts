67118754b6ed5b9d5fc1a0274ef8b60a98150d93
JR <70702656+julienEcole@users.noreply.github.com>
2023-04-29 16:20:41 +0200

    feat : route teste, plus qu'a ajouter les routes efficace

--- partieNodeJs/index.ts
+++ partieNodeJs/index.ts
@@ -5,49 +5,53 @@ config({ path: '../.env' });
 
 
 
-const app = express();
-const db = database.createConnection({
-    host: process.env.MYSQLDB_HOST_SERVER,
-    //port: Number(process.env.MYSQLDB_LOCAL_PORT),
-    user: process.env.MYSQLDB_USER,
-    password: process.env.MYSQLDB_ROOT_PASSWORD,
-    database: process.env.MYSQLDB_DATABASE
-});
 
-db.connect(
-    function (err) { 
-        if (err) { 
-            console.log("!!! Cannot connect !!! Error:");
-            throw err;
-        }
-        else
-        {
-           console.log("Connection established.");
-            //queryDatabase();
-        }
-    }
-);
+ 
 
-//db.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+// db.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 //     if (err) throw err;
 //     console.log('The solution is: ', rows[0].solution);
-//});
-
-console.log(db);
-
-db.end();
+// });
 
 async function startServer():Promise<void> {
-
-    console.log("TODO startup server")
+    
+    const db = database.createConnection({
+        host: process.env.MYSQLDB_HOST_SERVER,
+        //port: Number(process.env.MYSQLDB_LOCAL_PORT),
+        user: process.env.MYSQLDB_USER,
+        password: process.env.MYSQLDB_ROOT_PASSWORD,
+        database: process.env.MYSQLDB_DATABASE
+    });
+    
+    db.connect(
+        function (err) {
+            if (err) {
+                console.log("!!! Cannot connect to the database !!! Error:");
+                throw err;
+            }
+            else
+            {
+                console.log("Connection established with the database.");
+            }
+        }
+    );
+    console.log(db);
 }
 
-app.listen(process.env.PORT,function(){
-    console.log(`Serveur listening on port ${process.env.API_NODEJS_PORT}`);
+const app = express();
+app.get('/', (req, res) => {
+    res.send("What's up doc ?!");
 });
 
+app.get('/ping', function(req, res) {
+    res.send("pong");
+});
+//queryDatabase();
+app.listen(process.env.API_NODEJS_PORT,function(){
+    console.log(`server running : http://${process.env.API_NODEJS_BACK_HOST}:${process.env.API_NODEJS_PORT}`);
+});
 startServer().catch(function(err):void{
     console.log(err);
 });
 
-console.log("hello world");
+// console.log("hello world");
