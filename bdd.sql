660243b20b8663c9a5011dfe565b26be7ca239da
JR <70702656+julienEcole@users.noreply.github.com>
2023-05-06 11:43:16 +0200

    fix : ajout de l'attribut auto increment au clé primaire pour ne pas avoir a gérer ça

--- bdd.sql
+++ bdd.sql
@@ -1,36 +1,36 @@
 USE vecoleo;
 
 CREATE TABLE role_utilisateur (
-    role_utilisateur_id INT PRIMARY KEY NOT NULL,
+    role_utilisateur_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     titre TEXT NOT NULL,
     description TEXT
 );
 CREATE TABLE utilisateur (
-    utilisateur_id INT PRIMARY KEY NOT NULL,
+    utilisateur_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     mdp TEXT NOT NULL,
     mail TEXT NOT NULL,
     role_utilisateur_id INT NOT NULL REFERENCES role_utilisateur(role_utilisateur_id)
 );
 CREATE TABLE type_ticket (
-    type_ticket_id INT PRIMARY KEY NOT NULL,
+    type_ticket_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     titre TEXT NOT NULL,
     description TEXT
 );
 
 CREATE TABLE urgence (
-    urgence_id INT PRIMARY KEY NOT NULL,
+    urgence_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     titre TEXT NOT NULL,
     description TEXT
 );
 
 CREATE TABLE etat (
-    etat_id INT PRIMARY KEY NOT NULL,
+    etat_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     titre TEXT NOT NULL,
     description TEXT
 );
 
 CREATE TABLE ticket (
-    ticket_id INT PRIMARY KEY NOT NULL,
+    ticket_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     etat_id INT NOT NULL REFERENCES etat(etat_id),
     urgence_id INT NOT NULL REFERENCES urgence(urgence_id),
     type_ticket_id INT NOT NULL REFERENCES type_ticket(type_ticket_id),
@@ -38,7 +38,7 @@ CREATE TABLE ticket (
 );
 
 CREATE TABLE image (
-    image_id INT PRIMARY KEY NOT NULL,
+    image_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     fichier_image BLOB NOT NULL,
     description TEXT,
     nom TEXT NOT NULL
