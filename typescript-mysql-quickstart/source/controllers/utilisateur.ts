9d4e59b9cfc8f47904dc09b2a1566cc1c3372098
JR <70702656+julienEcole@users.noreply.github.com>
2023-05-09 23:32:00 +0200

    feat : modification du mdp et mail d'un utilisateur grace a son id, actuellement sans protection

--- typescript-mysql-quickstart/source/controllers/utilisateur.ts
+++ typescript-mysql-quickstart/source/controllers/utilisateur.ts
@@ -99,7 +99,7 @@ const getOneUserById = async (req: Request, res: Response, next: NextFunction) =
     logging.info(NAMESPACE, 'Getting one user by id.');
     if(!req.params.idUser){
         res.status(400);
-        res.send("erreur, les arguments doivent être le mail ou l'id de l'utilisateur");
+        res.send("erreur, les arguments doivent être l'id de l'utilisateur");
         return;
     }
     const query = `SELECT * FROM utilisateur WHERE utilisateur.utilisateur_id = ${req.params.idUser}`;
@@ -142,7 +142,7 @@ const getOneUserByMail = async (req: Request<{ mailUser: string}>, res: Response
     logging.info(NAMESPACE, 'Getting one user by id.');
     if(!(req.params && req.params.mailUser && typeof req.params.mailUser === "string")){
         res.status(400);
-        res.send("erreur, les arguments doivent être le mail ou l'id de l'utilisateur");
+        res.send("erreur, les arguments doivent être le mail de l'utilisateur");
         return;
     }
     const query = `SELECT * FROM utilisateur WHERE utilisateur.mail = '${req.params.mailUser}'`;
@@ -182,4 +182,76 @@ const getOneUserByMail = async (req: Request<{ mailUser: string}>, res: Response
         });
 };
 
-export default { createUser, getAllUsers, getOneUserById, getOneUserByMail };
+const updateOneUserById = async (req: Request, res: Response, next: NextFunction) => {
+    logging.info(NAMESPACE, 'updating one user by id.');
+    const isInt : RegExp = new RegExp("[0-9]*")
+    if(!req.params || !req.body || !req.params.idUser || !isInt.test(req.params.idUser)){
+        res.status(400);
+        res.send(`erreur, les arguments doivent être le mail ou l'id de l'utilisateur`); //\n req.params.idUser = ${req.params.idUser}
+        return;
+    }
+    
+    const mdp : string = req.body.mdp;
+    const mail : string = req.body.mail;
+    const isMail : RegExp = new RegExp(`(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))`)
+    if(!isMail.test(mail) && mdp.length < 8 ){
+        res.status(400);
+        res.send("le nouveau mail n'est pas au bon format ou le mot de passe n'est pas assez long");
+        return;
+    }
+    let query = `UPDATE utilisateur SET `;
+    if(mail){
+        if(mdp){
+            query += `mdp = "${mdp}", mail = "${mail}" `
+        }
+        else{
+            query+= `mail = "${mail}" `;
+        }
+    }
+    else if (mdp){
+        query += `mdp = "${mdp}" `;
+    }
+    else{
+        res.status(400);
+        res.send("veuillez envoyer un mdp et un mail dans l'update pour mettre a jour l'utilisateur.");
+        return;
+    }
+    query += `WHERE utilisateur.utilisateur_id = ${req.params.idUser}`
+    
+    logging.info(NAMESPACE,"ma query = ", query);
+
+    
+
+    Connect()
+        .then((connection) => {
+            Query(connection, query)
+                .then((results) => {
+                    logging.info(NAMESPACE, 'updating users: ', results);
+
+                    return res.status(200).json({
+                        results
+                    });
+                })
+                .catch((error) => {
+                    logging.error(NAMESPACE, error.message, error);
+                    return res.status(200).json({
+                        message: error.message,
+                        error
+                    });
+                })
+                .finally(() => {
+                    logging.info(NAMESPACE, 'Closing connection.');
+                    connection.end();
+                });
+        })
+        .catch((error) => {
+            logging.error(NAMESPACE, error.message, error);
+
+            return res.status(200).json({
+                message: error.message,
+                error
+            });
+        });
+};
+
+export default { createUser, getAllUsers, getOneUserById, getOneUserByMail,updateOneUserById };
--- typescript-mysql-quickstart/source/routes/utilisateur.ts
+++ typescript-mysql-quickstart/source/routes/utilisateur.ts
@@ -7,5 +7,6 @@ router.post('/create/utilisateur', express.json(), controller.createUser);
 router.get('/get/utilisateur', controller.getAllUsers);
 router.get("/get/utilisateur/id/:idUser", controller.getOneUserById);
 router.get("/get/utilisateur/mail/:mailUser", controller.getOneUserByMail);
+router.put("/put/utilisateur/:idUser", express.json(), controller.updateOneUserById);
 
 export = router;
