113dde538bdba1e920e0b94031215fe62695f493
Samalairbien <74256932+SamEPK@users.noreply.github.com>
2023-05-14 16:33:21 +0200

    Ajout front page "poster problème" et "réparateur proche"
    Le front des deux pages a été faits.

--- angular/vecoelo/src/app/app-routing.module.ts
+++ angular/vecoelo/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PostProblemeComponent } from './pages/post-probleme/post-probleme.component';
+import { ReparateurProcheComponent } from './pages/reparateur-proche/reparateur-proche.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'post-probleme', component: PostProblemeComponent },
+  { path: 'reparateur-proche', component: ReparateurProcheComponent }
 ];
 
 @NgModule({
