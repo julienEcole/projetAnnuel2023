228c421adaa01bb8fbe92602ab5281909445c9d9
JR <70702656+julienEcole@users.noreply.github.com>
2023-07-16 22:37:00 +0200

    fix : route patch & post avait une erreur dans la table utilisateur_atelier

--- typescript-mysql-quickstart/source/routes/vecoleo/profetionnel/utilisateur_atelier.ts
+++ typescript-mysql-quickstart/source/routes/vecoleo/profetionnel/utilisateur_atelier.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/get/utilisateur_atelier/idAtelier/:atelier_id", controller.getAllUtilisateurFromAtelier);
 router.get("/get/utilisateur_atelier/idUtilisateur/:utilisateur_id", controller.getAllAtelierFromUtilisateur);
 router.post("/post/utilisateur_atelier",express.json(), controller.createUtilisateur_Atelier);
-router.patch("/patch/atelier/:atelier_id", express.json(), controller.updateOneUtilisateur_AtelierById); //produit toujours erreur 403
-router.delete("/delete/atelier/:atelier_id",express.json(), controller.DeleteOneUtilisateur_AtelierById) //envoyer JSON car contiens une clef composé
+router.patch("/patch/utilisateur_atelier/:atelier_id", express.json(), controller.updateOneUtilisateur_AtelierById); //produit toujours erreur 403
+router.delete("/delete/utilisateur_atelier/:atelier_id",express.json(), controller.DeleteOneUtilisateur_AtelierById) //envoyer JSON car contiens une clef composé
 
 export = router;
