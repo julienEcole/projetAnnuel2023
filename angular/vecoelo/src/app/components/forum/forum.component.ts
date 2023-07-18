565a1bc5fbbcd9bafcbab757c48bd6711e1d6174
Samalairbien <74256932+SamEPK@users.noreply.github.com>
2023-07-18 02:09:06 +0200

    post probleme + edit user + delete user + get post

--- angular/vecoelo/src/app/components/forum/forum.component.ts
+++ angular/vecoelo/src/app/components/forum/forum.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ForumService } from './forum.service';
 
 @Component({
@@ -6,55 +6,59 @@ import { ForumService } from './forum.service';
   templateUrl: 'forum.component.html',
   styleUrls: ['forum.component.css']
 })
-export class ForumComponent {
-  constructor(private forumService: ForumService) { this.lastActivityDate = null; }
-  searchText: string = '';
-  filteredPosts: any[] = [];
-  lastActivityDate: string | null;;
+export class ForumComponent implements OnInit {
+  pseudo: string = '';
+  mail: string = '';
+  constructor(private forumService: ForumService) { }
 
-  get posts() {
-    // Récupérer les publications du service
-    const allPosts = this.forumService.getProblems();
-  
-    const sortedPosts = allPosts.sort((a, b) => {
-      const lastActivityDateA = new Date(this.getLastActivityDate(a));
-      const lastActivityDateB = new Date(this.getLastActivityDate(b));
-      const publicationDateA = new Date(a.timestamp);
-      const publicationDateB = new Date(b.timestamp);
-  
-      if (isNaN(lastActivityDateA.getTime())) {
-        return 1; // Gérer le cas où la dernière activité de la publication A est invalide
-      }
-  
-      if (isNaN(lastActivityDateB.getTime())) {
-        return -1; // Gérer le cas où la dernière activité de la publication B est invalide
-      }
-  
-      // Tri par la dernière activité
-      if (lastActivityDateB.getTime() !== lastActivityDateA.getTime()) {
-        return lastActivityDateB.getTime() - lastActivityDateA.getTime();
-      }
-  
-      // Tri par la dernière publication si les dernières activités sont les mêmes
-      return publicationDateB.getTime() - publicationDateA.getTime();
-    });
-  
-    return sortedPosts;
+  posts: any[] = [];
+
+  ngOnInit(): void {
+
+    this.loadPosts();
   }
- 
-  deletePost(postId: string) {
+
+  loadPosts(): void {
+    this.forumService.getProblems().subscribe(
+      (response: any) => {
+        if (Array.isArray(response.results)) {
+          this.posts = response.results;
+  
+          for (const post of this.posts) {
+            let id = post.utilisateur_id;
+  
+            this.forumService.getOneUserById(id).subscribe(
+              (userResponse: any) => {
+                const utilisateur = userResponse.results[0];
+                post.pseudo = utilisateur.pseudo;
+                console.log('Chargement des réponses du post:', post);
+              },
+              (error: any) => {
+                console.log('Une erreur s\'est produite lors de la récupération des informations utilisateur :', error);
+              }
+            );
+          }
+  
+          console.log('Chargement des publications réussi:', this.posts);
+        } else {
+          console.error('Erreur lors du chargement des publications: La réponse.results n\'est pas un tableau.', response);
+        }
+      },
+      (error: any) => {
+        console.error('Erreur lors du chargement des publications:', error);
+        // Traitez l'erreur de chargement des publications si nécessaire
+      }
+    );
+  }
+  
+
+
+
+  deletePost(postId: string): void {
     this.forumService.deletePost(postId);
+    this.loadPosts();
   }
-  filterPosts() {
-    if (this.searchText) {
-      this.filteredPosts = this.posts.filter(post =>
-        post.objet.toLowerCase().includes(this.searchText.toLowerCase()) ||
-        (post.autreProbleme && post.autreProbleme.toLowerCase().includes(this.searchText.toLowerCase()))
-      );
-    } else {
-      this.filteredPosts = this.posts;
-    }
-  }
+
   getLastActivityDate(post: any): string {
     const replies = post.replies;
     if (replies && replies.length > 0) {
@@ -63,15 +67,11 @@ export class ForumComponent {
       return "Aucune activité";
     }
   }
-  
-
 
   formatDateTime(dateTime: string): string {
     if (!dateTime) {
-      return ""; // Gérer le cas où dateTime est vide ou non défini
+      return "";
     }
-
-
 
     const date = new Date(dateTime);
     date.setHours(date.getHours() + 2);
@@ -86,11 +86,8 @@ export class ForumComponent {
     };
     const formatter = new Intl.DateTimeFormat('fr-FR', options);
     if (isNaN(date.getTime())) {
-      return ""; // Gérer le cas où dateTime n'est pas une date valide
+      return "";
     }
     return formatter.format(date);
   }
-  
-
-
 }
--- angular/vecoelo/src/app/components/login/user.service.ts
+++ angular/vecoelo/src/app/components/login/user.service.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class UserService {
-  private baseUrl = 'http://localhost:1337';
+  private baseUrl = 'http://localhost:3999';
   private estConnecte: boolean = false;
   private pseudo: string = '';
   private email: string = '';
@@ -16,8 +16,8 @@ export class UserService {
   private nom: string = '';
   private prenom: string = '';
 
-  constructor(private http: HttpClient, private router : Router) { }
- 
+  constructor(private http: HttpClient, private router: Router) { }
+
   registerUser(pseudo: string, nom: string, prenom: string, email: string, password: string): Observable<boolean> {
     const user = {
       pseudo,
@@ -58,19 +58,19 @@ export class UserService {
           console.log('Échec de la connexion : email ou mot de passe incorrect');
           return false;
         }
-        
+
       }),
       catchError(() => {
         console.log('Échec de la connexion : email ou mot de passe incorrect');
         return of(false);
       })
-      
+
     );
-    
+
   }
   estUtilisateurConnecte(): boolean {
     this.estConnecte = localStorage.getItem('estConnecte') === 'true';
-    if(this.estConnecte) {
+    if (this.estConnecte) {
       return this.estConnecte;
     } else {
       return false;
@@ -97,7 +97,7 @@ export class UserService {
 
 
   updateOneUserByMail(email: string, user: any): Observable<any> {
-    const url = `utilisateur/put/utilisateur/mail/${email}`;
+    const url = `${this.baseUrl}/utilisateur/put/utilisateur/mail/${email}`;
     return this.http.put(url, user);
   }
 
@@ -105,39 +105,28 @@ export class UserService {
     this.estConnecte = this.estUtilisateurConnecte();
     if (this.estConnecte) {
       this.pseudo = localStorage.getItem('pseudo') || "";
-      return this.pseudo; 
+      return this.pseudo;
     } else {
       return '';
     }
   }
-  
-updatePassword(id: string, password: string): Observable<any> {
-  const user = {
-    password: password
-  };
-  const url = `${this.baseUrl}/utilisateur/patch/utilisateur/${id}`;
-  return this.http.patch(url, user);
-}
 
-updateEmail(id: string, email: string): Observable<any> {
-  const user = {
-    mail: email
-  };
-  const url = `${this.baseUrl}/utilisateur/patch/utilisateur/${id}`;
-  return this.http.patch(url, user);
-}
+  updateUser(id: string, updatedData: any): Observable<any> {
+    const url = `${this.baseUrl}/utilisateur/patch/utilisateur/${id}`;
+    localStorage.setItem('pseudo', updatedData.pseudo);
+    localStorage.setItem('email', updatedData.mail);
+    window.location.reload();
+    return this.http.put(url,updatedData);
+  }
 
-updatePseudo(id: string, pseudo: string): Observable<any> {
-  const user = {
-    pseudo: pseudo
-  };
-  const url = `${this.baseUrl}/utilisateur/patch/utilisateur/${id}`;
-  return this.http.patch(url, user);
-}
-deleteOneUserById(id: string): Observable<any> {
-  const url = `${this.baseUrl}/utilisateur/delete/utilisateur/id/${id}`;
-  return this.http.delete(url);
-}
+  deleteOneUserById(id: string): Observable<any> {
+    const url = `${this.baseUrl}/utilisateur/delete/utilisateur/id/${id}`;
+    localStorage.removeItem('estConnecte');
+    localStorage.removeItem('pseudo');
+    this.estConnecte = false;
+    this.router.navigate(['/home']);
+    return this.http.delete(url);
+  }
 
 
   deconnexion(): void {
