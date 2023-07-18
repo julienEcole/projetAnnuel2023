6d107c070698663dbfffdffc9d843984c080a61b
JR <70702656+julienEcole@users.noreply.github.com>
2023-07-18 12:07:04 +0200

    fix : replacement de put par patch dans les routes de ticket.ts

--- typescript-mysql-quickstart/source/routes/ticket.ts
+++ typescript-mysql-quickstart/source/routes/ticket.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/get/ticket",controller.getAllTicket);
 router.get("/get/ticket/:ticket_id", controller.getOneTicketById);
 router.post("/post/ticket",express.json(), controller.createTicket);
-router.put("/patch/ticket/:ticket_id", express.json(), controller.updateOneTicketById);
+router.patch("/patch/ticket/:ticket_id", express.json(), controller.updateOneTicketById);
 router.delete("/delete/ticket/:ticket_id", controller.DeleteOneTicketById)
 
 export = router;
